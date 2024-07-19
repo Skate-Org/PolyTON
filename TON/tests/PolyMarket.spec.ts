6751757f4634b245ff59f1878ea017d8f7579175
phidung-hub <dungho01092002@gmail.com>
2024-07-19 17:38:59 +0800

    stateless polymarket contracts

--- TON/tests/PolyMarket.spec.ts
+++ TON/tests/PolyMarket.spec.ts
@@ -1,20 +1,23 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano, beginCell, Cell, Dictionary } from '@ton/core';
+import { toNano, address } from '@ton/core';
 import '@ton/test-utils';
 import { PolyMarket } from '../build/PolyMarket/tact_PolyMarket';
+import { SkateGateway } from '../build/SkateGateway/tact_SkateGateway';
 
 describe('PolyMarket', () => {
   let blockchain: Blockchain;
   let deployer: SandboxContract<TreasuryContract>;
   let polyMarket: SandboxContract<PolyMarket>;
-  let relayerPublicKey = BigInt('0x072aa6ab487813c8763e8564cf74356e351280cff6380bf28a845259a6e90433');
+  let jetton_USDT = address("EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs");
+  let skateGateway: SandboxContract<SkateGateway>;
+  const relayerPublicKey = BigInt('0x072aa6ab487813c8763e8564cf74356e351280cff6380bf28a845259a6e90433');
 
   beforeEach(async () => {
     blockchain = await Blockchain.create();
 
     deployer = await blockchain.treasury('deployer');
-
-    polyMarket = blockchain.openContract(await PolyMarket.fromInit(deployer.address, relayerPublicKey));
+    skateGateway = blockchain.openContract(await SkateGateway.fromInit(deployer.address, relayerPublicKey));
+    polyMarket = blockchain.openContract(await PolyMarket.fromInit(skateGateway.address, jetton_USDT));
 
     const deployResult = await polyMarket.send(
       deployer.getSender(),
@@ -22,7 +25,6 @@ describe('PolyMarket', () => {
         value: toNano('0.1'),
       },
       {
-        // @ts-ignore
         $$type: 'Deploy',
         queryId: 0n,
       },
@@ -34,10 +36,23 @@ describe('PolyMarket', () => {
       deploy: true,
       success: true,
     });
-  });
+  })
 
   it('should deploy', async () => {
     // the check is done inside beforeEach
     // blockchain and skateGateway are ready to use
-  });
+  })
+
+  it('should init correctly', async () => {
+    const jettonData = await polyMarket.getGetWalletData();
+    console.log(jettonData);
+    const gateway = await polyMarket.getGateway();
+    expect(gateway.toString()).toEqual(skateGateway.address.toString());
+
+    const lastTaskId = await polyMarket.getLastTaskId();
+    expect(lastTaskId).toEqual(0n);
+
+    // const betBook = await polyMarket.getBetBook();
+    // expect(betBook.size).toEqual(0);
+  })
 });
